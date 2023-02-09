520cc63699b1b02e1bd7a3be41d134712d7a4491
Daniel Young <danyoungmusic93@gmail.com>
2023-02-09 10:01:08 -0500

    [animation][test] Setup test data changes
    Sets up 2 files for player data which changes at 2.5s intervals for
    testing the animation library added to the ScoreCard components in
    future CLs.
    
    Solves: ability to test card animations
    
    Tests: N/A

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
+import { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import players from './data.json';
 import Container from 'react-bootstrap/Container';
-
+import players1 from './data1.json';
+import players2 from './data2.json';
 import './App.css';
 import ScoreCard from './ScoreCard';
 
+const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+
+const setPlayers = async (callback) => {
+  callback(players1);
+  await sleep(2500);
+  callback(players2);
+  setTimeout(() => setPlayers(callback), 2500);
+};
+
 function App() {
-	return (
-		<div className='App vh-100'>
-			<Container id='main-container' className='mx-auto d-flex justify-content-center'>
-				<div className='flex-col align-items-center'>
-					<h1 id='page-title' title='GO BIRDS!'>
-						Leaderboard
-					</h1>
-					<div id='results'>
-						{players.map((player) => {
-							console.log('player:');
-							return <ScoreCard player={player} />;
-						})}
-					</div>
-				</div>
-			</Container>
-		</div>
-	);
+  const [players, setPlayersState] = useState([players1]);
+
+  useEffect(() => {
+    setPlayers(setPlayersState);
+  }, []);
+
+  return (
+    <div className='App vh-100'>
+      <Container
+        id='main-container'
+        className='mx-auto d-flex justify-content-center'
+      >
+        <div className='flex-col align-items-center'>
+          <h1 id='page-title' title='GO BIRDS!'>
+            Leaderboard
+          </h1>
+          <div id='results'>
+            {players.map((player) => {
+              console.log('player:');
+              return <ScoreCard player={player} />;
+            })}
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
