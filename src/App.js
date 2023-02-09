8b3de06014deb08ed83a2225b1dde1b107fb1f20
Daniel Young <danyoungmusic93@gmail.com>
2023-02-08 19:35:43 -0500

    [aws][integration] Add S3 SDK call to get players
    Adds AWS SDK for UnauthorizedCognitoIdentity authorization to connect to
    S3 for the current_scores.json file. Does not handle any errors, so
    probably will break on gameday.
    
    Solves: Checks for score updates every 5 seconds
    
    Tests: N/A (we don't test javascript, we just let it fail)

--- src/App.js
+++ src/App.js
@@ -6,57 +6,79 @@ import players1 from './data1.json';
 import players2 from './data2.json';
 import './App.css';
 import './ScoreCard.css';
+const AWS = require('aws-sdk');
 
-const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
-
-const setPlayers = async (callback) => {
-	callback(players1);
-	await sleep(2500);
-	callback(players2);
-	setTimeout(() => setPlayers(callback), 2500);
-};
+AWS.config.region = 'us-east-1'; // Region
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+  IdentityPoolId: 'us-east-1:70e8bed7-eaf4-40e8-83fc-d03db725c61f',
+});
+const client = new AWS.S3();
 
 const ScoreCard = forwardRef(({ props, ref }) => {
-	return (
-		<div className='score-card' ref={ref}>
-			<div className='d-flex justify-content-between'>
-				<div className='sc-name'>{props.name}</div>
-				<div className='sc-score align-self-end'>{props.score}</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className='score-card' ref={ref}>
+      <div className='d-flex justify-content-between'>
+        <div className='sc-name'>{props.name}</div>
+        <div className='sc-score align-self-end'>{props.score}</div>
+      </div>
+    </div>
+  );
 });
 
+const _getPropSheetScores = () => {
+  const bucket = 'prop-sheet';
+  const key = 'current_scores.json';
+  return new Promise((resolve, reject) => {
+    client.getObject({ Bucket: bucket, Key: key }, (err, res) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(JSON.parse(res.Body.toString('utf-8')));
+      }
+    });
+  });
+};
+
+const getPropSheetScores = (callback) => {
+  _getPropSheetScores().then((data) => {
+    callback(data);
+  });
+  setTimeout(() => getPropSheetScores(callback), 5000);
+};
+
 function App() {
-	const [players, setPlayersState] = useState([players1]);
+  const [players, setPlayers] = useState([]);
 
-	useEffect(() => {
-		setPlayers(setPlayersState);
-	}, []);
+  useEffect(() => {
+    getPropSheetScores(setPlayers);
+  }, []);
 
-	return (
-		<div className='App vh-100'>
-			<Container id='main-container' className='mx-auto d-flex justify-content-center'>
-				<div id='main-col' className='flex-col align-items-center'>
-					<div id='page-title' title='GO BIRDS'>
-						<h2>Super Bowl LVII</h2>
-						<h1>Leaderboard</h1>
-					</div>
-					<div className='items'>
-						{/* <FlipMove> */}
-						{players.map((player, index) => {
-							return (
-								<div key={player[index]}>
-									<ScoreCard props={player} />
-								</div>
-							);
-						})}
-						{/* </FlipMove> */}
-					</div>
-				</div>
-			</Container>
-		</div>
-	);
+  return (
+    <div className='App vh-100'>
+      <Container
+        id='main-container'
+        className='mx-auto d-flex justify-content-center'
+      >
+        <div id='main-col' className='flex-col align-items-center'>
+          <div id='page-title' title='GO BIRDS'>
+            <h2>Super Bowl LVII</h2>
+            <h1>Leaderboard</h1>
+          </div>
+          <div className='items'>
+            {/* <FlipMove> */}
+            {players.map((player, index) => {
+              return (
+                <div key={player[index]}>
+                  <ScoreCard props={player} />
+                </div>
+              );
+            })}
+            {/* </FlipMove> */}
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
