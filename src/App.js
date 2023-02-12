f4bdd6a8924375bbb3ef4e7677b8543d32a17206
Christina Giancola <christinamgiancola@gmail.com>
2023-02-11 20:29:33 -0500

    Centered score number when name breaks to two lines

--- src/App.js
+++ src/App.js
@@ -8,69 +8,64 @@ const AWS = require('aws-sdk');
 
 AWS.config.region = 'us-east-1'; // Region
 AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-  IdentityPoolId: 'us-east-1:70e8bed7-eaf4-40e8-83fc-d03db725c61f',
+	IdentityPoolId: 'us-east-1:70e8bed7-eaf4-40e8-83fc-d03db725c61f',
 });
 const client = new AWS.S3();
 
 const _getPropSheetScores = () => {
-  const bucket = 'prop-sheet';
-  const key = 'current_scores.json';
-  return new Promise((resolve, reject) => {
-    client.getObject({ Bucket: bucket, Key: key }, (err, res) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(JSON.parse(res.Body.toString('utf-8')));
-      }
-    });
-  });
+	const bucket = 'prop-sheet';
+	const key = 'current_scores.json';
+	return new Promise((resolve, reject) => {
+		client.getObject({ Bucket: bucket, Key: key }, (err, res) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(JSON.parse(res.Body.toString('utf-8')));
+			}
+		});
+	});
 };
 
 const getPropSheetScores = (callback) => {
-  _getPropSheetScores().then((data) => {
-    callback(data);
-  });
-  setTimeout(() => getPropSheetScores(callback), 5000);
+	_getPropSheetScores().then((data) => {
+		callback(data);
+	});
+	setTimeout(() => getPropSheetScores(callback), 5000);
 };
 
 function App() {
-  const [players, setPlayers] = useState([]);
+	const [players, setPlayers] = useState([]);
 
-  useEffect(() => {
-    getPropSheetScores(setPlayers);
-  }, []);
+	useEffect(() => {
+		getPropSheetScores(setPlayers);
+	}, []);
 
-  return (
-    <div className='App vh-100'>
-      <Container
-        id='main-container'
-        className='mx-auto d-flex justify-content-center'
-      >
-        <div id='main-col' className='flex-col align-items-center'>
-          <div id='page-title' title='GO BIRDS'>
-            <h2>Super Bowl LVII</h2>
-            <h1>Leaderboard</h1>
-          </div>
-          <div id='items'>
-            <FlipMove>
-              {players.map((player) => {
-                return (
-                  <div className='score-card' key={player.email}>
-                    <div className='d-flex justify-content-between'>
-                      <div className='sc-name'>{player.name}</div>
-                      <div className='sc-score align-self-end'>
-                        {player.score}
-                      </div>
-                    </div>
-                  </div>
-                );
-              })}
-            </FlipMove>
-          </div>
-        </div>
-      </Container>
-    </div>
-  );
+	return (
+		<div className='App vh-100'>
+			<Container id='main-container' className='mx-auto d-flex justify-content-center'>
+				<div id='main-col' className='flex-col align-items-center'>
+					<div id='page-title' title='GO BIRDS'>
+						<h2>Super Bowl LVII</h2>
+						<h1>Leaderboard</h1>
+					</div>
+					<div id='items'>
+						<FlipMove>
+							{players.map((player) => {
+								return (
+									<div className='score-card' key={player.email}>
+										<div className='d-flex justify-content-between'>
+											<div className='sc-name'>{player.name}</div>
+											<div className='sc-score align-self-center'>{player.score}</div>
+										</div>
+									</div>
+								);
+							})}
+						</FlipMove>
+					</div>
+				</div>
+			</Container>
+		</div>
+	);
 }
 
 export default App;
